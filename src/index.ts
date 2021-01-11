86f1304b43ab8882161d18bd7bd7382fc9ac8db9
leo <leo_9_t@hotmail.com>
2021-01-11 16:38:50 -0400

    ADDED seeder on dev mode

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ require('dotenv').config()
 import {app, bootstrap} from './app'
 import {AddressInfo} from 'net'
 import sequelize from './utils/DB';
+import { SeedSuperadmin } from './utils/Seeder';
 
 bootstrap()
 .then(() => sequelize.sync())
+.then(() => {
+    if (process.env.DEV) {
+        SeedSuperadmin()
+    }
+})
 .then(() => {
     const server = app.listen(parseInt(process.env.PORT || '5000') || 5000, '0.0.0.0', () => {
         const {port, address} = server.address() as AddressInfo;
--- src/utils/Seeder.ts
+++ src/utils/Seeder.ts
@@ -1,19 +1,14 @@
-import { UserModel, USER_ROLE_ENUM } from "../models/user.model"
+import { UserModel } from "../models/user.model"
 import { hash, hashSync } from "bcrypt"
 
 export const SeedSuperadmin = () => {
     const userData = {
-        name: "Admin 1",
-        location: "Uk",
-        email: "super_admin@mail.com",
-        password: hashSync("5ct0cz", 8),
-        mobileNumber: "0",
-        age: "18",
-        role: USER_ROLE_ENUM.SUPER_ADMIN,
-        profilePic: "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+        "nickname": "Chef1",
+        "password": "123456",
+        "emailAddress": "chef1@mail.com"
     }
     return UserModel.findOrCreate({
-        where: { email: 'super_admin@mail.com'},
+        where: { nickname: userData.nickname },
         defaults: userData
     })
 }
